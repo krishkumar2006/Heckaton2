652685dc2d79b3262987abe8867d41631ec74e57
Krish kumar <160249989+krishkumar2006@users.noreply.github.com>
2024-12-17 00:41:33 +0500

    added new files

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,14 @@
+import ShopPage from "../../components/shop-header/shop-header"
+import ShopMain from "../../components/shop-main/shop-main"
+
+
+const Shop = () => {
+  return (
+    <>
+    <ShopPage />
+    <ShopMain />
+    </>
+  )
+}
+
+export default Shop
